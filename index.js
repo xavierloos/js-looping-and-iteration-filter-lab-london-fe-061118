1dc967791675edc5d937d56a7ecb8ab1468c2877
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-31 18:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file
+// Code your solution in this filed
+let driver= function findMatching(drivers){
+  return findMatching.filter(drivers==Scuber )
+}
+function fuzzyMatch(drivers){
+  drivers.filter(driver);
+}
